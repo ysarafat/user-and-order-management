1b82a35ef244139425712ae1c23acf8044b2f46a
Yeasir Arafat <ysarafat.bd@gmail.com>
2023-11-22 22:08:04 +0600

    .env config

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
+import mongoose from "mongoose";
 import app from "./app";
-
+import config from "./config";
+const PORT = config.port || 5000;
 async function main() {
   try {
-    app.listen(5000, () => {
-      console.log("server is running");
+    mongoose.connect(config.database_url as string).then(() => {
+      console.log("Database connected");
+    });
+    app.listen(PORT, () => {
+      console.log(`server is running on port ${PORT}`);
     });
   } catch (error) {
     console.log(error);
