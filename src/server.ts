33599903b5cb72cb108125e5e688d9886558eb4f
Yeasir Arafat <ysarafat.bd@gmail.com>
2023-11-22 22:42:41 +0600

    setup linting

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,18 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
-const PORT = config.port || 5000;
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
+const PORT = config.port || 5000
 async function main() {
   try {
     mongoose.connect(config.database_url as string).then(() => {
-      console.log("Database connected");
-    });
+      console.log('Database connected')
+    })
     app.listen(PORT, () => {
-      console.log(`server is running on port ${PORT}`);
-    });
+      console.log(`server is running on port ${PORT}`)
+    })
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
 }
 
-main();
+main()
